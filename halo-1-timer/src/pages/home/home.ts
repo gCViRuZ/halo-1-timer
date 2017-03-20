2dd8627a425bddda36c3b5a68ec3b8c8d387bea7
Russell Colwell <russellcolwell@Russells-MacBook-Pro.local>
2017-03-19 21:05:14 -0500

    started base timer page

--- halo-1-timer/src/pages/home/home.ts
+++ halo-1-timer/src/pages/home/home.ts
@@ -8,8 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  time:string = "00:00"
+  tabBarElement: any;
   constructor(public navCtrl: NavController) {
-
+    this.tabBarElement = document.querySelector('.tabbar');
+    this.tabBarElement.style.display = 'none';
   }
 
 }
